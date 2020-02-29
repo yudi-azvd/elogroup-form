a1cac43d482fa8292dce72042d796a6211df8bd5
Yudi Yamane <yudiyamane@gmail.com>
2020-02-29 17:12:23 -0300

    added some fields and draft on general style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import logo from './assets/logo.svg'
+
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(event.target.name.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+          <p>font-family: flexo bold  </p>
+      <img id="logo" src={logo} alt="EloGroup"/>
+      
+      <form onSubmit={handleSubmit}>
+        <div className="control-group">
+          <input name="name" type="text" placeholder="Nome" />
+        </div>
+        
+        <div className="control-group">
+          <input name="phone" type="text" placeholder="Número de celular" />
+        </div>
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
