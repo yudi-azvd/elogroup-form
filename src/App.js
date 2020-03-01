3d613962cfeb3b3b27cda38fdbe38ac22dffa5dc
Yudi Yamane <yudiyamane@gmail.com>
2020-02-29 21:03:34 -0300

    trying to add social media options

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { FaFacebookSquare, FaInstagram, FaLinkedin} from 'react-icons/fa';
+// import {AiFillInstagram} from 'react-icons/ai';
 
 import './App.css';
 
 import logo from './assets/logo.svg'
 
 function App() {
+  const [hasSocialMedia, setHasSocialMedia] = useState(!true)
+
   function handleSubmit(event) {
     event.preventDefault()
     console.log(event.target.name.value)
   }
 
+  function handleHasSocialMediaChange(event) {
+    if (event.target.id === 'yes') {
+      setHasSocialMedia(true)
+    }
+    else {
+      setHasSocialMedia(false)
+    }
+  }
+
   return (
     <div className="content">
-          <p>font-family: flexo bold  </p>
       <img id="logo" src={logo} alt="EloGroup"/>
       
       <form onSubmit={handleSubmit}>
         <div className="control-group">
-          <input name="name" type="text" placeholder="Nome" />
+          <label htmlFor="name">Nome</label>
+          <input id="name" name="name" type="text" placeholder="Maria José da Silva" />
         </div>
         
         <div className="control-group">
-          <input name="phone" type="text" placeholder="Número de celular" />
+          <label htmlFor="phone">Telefone</label>
+          <input id="phone" name="phone" type="text" placeholder="Número de celular" />
         </div>
 
-        <button type="submit">Enviar</button>
+        {/* <hr/> */}
+        <div className="control-group social-media">
+          <div className="question">
+            <span className="has-social-media-question">Possui redes sociais?</span>
+
+            <div className="yes-or-no">
+              <div className="yes">
+                <input type="radio" name="hasSocialMedia" id="yes" onChange={handleHasSocialMediaChange} />
+                <label htmlFor="yes">Sim</label>
+              </div>
+              <div className="no">
+                <input type="radio" name="hasSocialMedia" id="no" onChange={handleHasSocialMediaChange} />
+                <label htmlFor="no">Não</label>
+              </div>
+            </div>
+          </div>
+
+          { hasSocialMedia ? (
+          <div>
+            <input type="radio" id="facebook" name="facebook" value="facebook"/>
+            <label htmlFor="facebook">Facebook</label>
+          </div> 
+          ) 
+          : <span id="no-social-media">Não tenho mídias sociais</span>}
+            
+          </div>
+
+        <button type="submit">ENVIAR</button>
       </form>
     </div>
   );
