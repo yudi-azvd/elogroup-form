7d560350f7c9e56fec5dbd7899372975fd550c96
Yudi Yamane <yudiyamane@gmail.com>
2020-03-02 15:42:47 -0300

    removed console.log

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './styles.css';
 
 export default function Checkbox({label, onChange, checked }) {
-  console.log('CRIEI CHECKBOX')
   return (
     <div className="checkbox-div">
       <input type="checkbox" id={label} name={label} onChange={onChange} checked={checked} />
