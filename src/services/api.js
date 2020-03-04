56773a14269b9fa7f388494f8b0b39d5fbc7530b
Yudi Yamane <yudiyamane@gmail.com>
2020-03-04 14:17:42 -0300

    returned .env to source control

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,11 @@ import axios from 'axios'
 
 console.log(process.env.REACT_APP_API_URL)
 
+let baseURL
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:8080'
+}
+
 const api = axios.create({
   // API_URL só é definido em ambiente de produção
   baseURL:  process.env.REACT_APP_API_URL || 'http://localhost:8080'
