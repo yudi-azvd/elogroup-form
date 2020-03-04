85475c776363f6d8a6898c6a16842ad93b9b533f
Yudi Yamane <yudiyamane@gmail.com>
2020-03-04 13:45:19 -0300

    test .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
-// import 'dotenv/config'
+import 'dotenv/config'
 
 import axios from 'axios'
 
-console.log(process.env.API_URL)
+console.log(process.env.REACT_APP_API_URL)
 
 const api = axios.create({
   // API_URL só é definido em ambiente de produção
-  baseURL:  process.env.API_URL || 'http://localhost:8080'
+  baseURL:  process.env.REACT_APP_API_URL || 'http://localhost:8080'
 })
 
 export default api
