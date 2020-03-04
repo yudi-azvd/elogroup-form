c72d51634143e70d2256457e46817efed20d5629
Yudi Yamane <yudiyamane@gmail.com>
2020-03-04 13:07:35 -0300

    added process.env.API_URL variable

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import axios from 'axios'
 
 const api = axios.create({
+  // API_URL só é definido em ambiente de produção
   baseURL:  process.env.API_URL || 'http://localhost:8080'
 })
 
