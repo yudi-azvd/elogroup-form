d2ee30955911b69367788ddefcb5ed00f7724e38
Yudi Yamane <yudiyamane@gmail.com>
2020-03-04 13:15:22 -0300

    testing .env var

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import 'dotenv/config'
 
 import axios from 'axios'
 
+console.log(process.env.API_URL)
+
 const api = axios.create({
   // API_URL só é definido em ambiente de produção
   baseURL:  process.env.API_URL || 'http://localhost:8080'
